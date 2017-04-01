4ee592879fd0cb579151df82f019644efe98a439
Alex Dao <azd2@duke.edu>
2017-04-01 18:15:07 -0400

    Add additional commands

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const execSync = require('child_process').execSync;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+let buffer = '';
+let prev = '';
+
 app.get('/', function(req, res) {
     res.send('<h1>Hello world</h1>');
-}); 
+});
 
 io.origins('*:*');
 io.on('connection', function(socket){
@@ -15,13 +18,41 @@ io.on('connection', function(socket){
 
   // Voice message came in
   socket.on('message', function(msg){
-    console.log('Voice: ' + msg);
+    if (msg === 'erase') {
+      erase();
+    }
+    else if (msg === 'analyze') {
+      analyze();
+      clear();
+    }
+    else if (msg === 'clear') {
+      clear();
+    }
+    else {
+      buffer += prev;
+      prev = msg + '. ';
+    }
   });
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
+// Speaker takes back his most recent sentence
+function erase() {
+  prev = '';
+}
+
+// Speaker has finished speaking
+function analyze() {
+  console.log('Voice: ' + buffer + prev);
+}
+
+function clear() {
+  buffer = '';
+  prev = '';
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
