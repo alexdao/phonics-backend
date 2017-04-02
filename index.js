a45bc4f1b9339419496979555c8da04ac2657535
Alex Dao <azd2@duke.edu>
2017-04-01 23:27:21 -0400

    Decode base 64 image

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = require('express')();
 const execSync = require('child_process').execSync;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const fs = require('fs');
 
 let buffer = '';
 let prev = '';
@@ -34,10 +35,24 @@ io.on('connection', function(socket){
     }
   });
 
+  socket.on('sending_image', function(msg){
+    decodeBase64(msg.image);
+    console.log('Received image!');
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
+
+function decodeBase64(base64Data) {
+  fs.writeFile("out.png", base64Data, 'base64', function(err) {
+    if (error != null) {
+      console.log('Error ' + err);
+    }
+  });
+}
 
 // Speaker takes back his most recent sentence
 function erase() {
